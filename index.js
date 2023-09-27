74b0dc1ff92908015dac5879e1a73ecafe53d156
bur1to <daniil.dedenok@gmail.com>
2023-09-27 20:58:30 +0300

    add users and goods routers

---
+++ index.js
@@ -0,0 +1,27 @@
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const {
+  userRouter,
+  goodsRouter
+} = require('./routers/index');
+
+mongoose.set('strictQuery', true);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+mongoose.connect('mongodb://127.0.0.1:27017/storeDB', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Database successfully connected'))
+  .catch((err) => console.log(err));
+
+app.use('/users', userRouter);
+app.use('/goods', goodsRouter);
+
+app.listen(3001, () => {
+  console.log('Server is running...');
+});
